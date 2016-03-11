b960ed73f20e2c51fe2c477e881d2db6e7529289
Jose Mugaburu <mugab686@gmail.com>
2016-03-11 15:05:39 -0500

    adds solution for juggling, but need to fix it still

--- nodeschool/jugglingAsync.js
+++ nodeschool/jugglingAsync.js
@@ -7,42 +7,20 @@ var totalData = [];
 var collect = function(urls){
   var data = '';
 
-
-
   for (var i = 0 ; i < urls.length; i++)
   {
-    //console.log(urls[i]);
     http.get(urls[i], (res) => {
-      //res.setEncoding('utf-8');
-      // res.on('data', (chunk) => {
-      //   data += chunk;
-      //   //console.log(data);
-      // });
-      // res.on('end', () => {
-      //   totalData.push(data);
-      //   // callback(null, totalData);
-      // });
-      // res.on('error', (err)=>{
-      //   console.error('Error: ' + err.message);
-      //   // callback(err, null);
-      // });
       res.pipe(bl(function(err,data){
         if (err) return console.error(err.message);
-
         totalData[i] = data.toString();
         printResults(totalData[i]);
-
       }));
-
     });
   }
 }
 
-collect(urls);
-
-
-function printResults(item, index){
-
+function printResults(item){
     console.log(item);
-
 }
+
+collect(urls);
