b6db85a2910b90e4d009f358123796ceab192408
Jose Mugaburu <mugab686@gmail.com>
2016-04-27 17:26:14 -0400

    completes exercises for web sockets training

---
+++ node-with-KS/10.server.js
@@ -0,0 +1,76 @@
+function handleHTTP(req, res){
+    if(req.method === "GET"){
+        if(/^\/\d+(?=$|[\/?#])/.test(req.url)){
+            req.addListener("end", function () {
+                req.url = req.url.replace(/^\/(\d+).*$/,"/$1.html")
+                static_files.serve(req, res);
+            });
+            req.resume();
+        } 
+        else if (req.url === "/jquery.js"){
+             static_files.serve(req, res); 
+        } 
+        else {
+            res.writeHead(403);
+            res.end("Not here. Move on!");            
+        }
+        
+    } else {
+        res.writeHead(403);
+        res.end("Not here. Move on!");
+    }
+}
+
+function handleIO(socket){
+    function disconnect(){
+        clearInterval(intv);
+        console.log("client disconnected");
+    }
+    
+    console.log("Client Connected");
+    socket.on("disconnect", disconnect);
+    
+    var intv = setInterval(function(){
+        socket.emit("hello", Math.random());
+    }, 1000);
+    
+    
+    socket.on("foo", function(msg){
+       socket.broadcast.emit("messages", msg); //everybody else, besides currently connected
+       //io.broadcast //send to everyone
+    });
+    
+    
+    socket.on("spy", function(x,y){
+       socket.broadcast.emit("spy", {
+           x: x,
+           y: y
+        }); 
+    });
+}
+
+
+var host = "localhost";
+var port = 8006;
+
+var http = require('http');
+var http_serv = http.createServer(handleHTTP).listen(port, host);
+
+var ASQ = require("asynquence");
+var node_static = require("node-static");
+
+var static_files = new node_static.Server(__dirname);
+var io = require('socket.io').listen(http_serv);
+
+io.on("connection", handleIO);
+
+io.configure(function(){
+   io.enable("browser client minification"); 
+   io.enable("browser client etag"); //caching
+   io.set("log level", 1); //reduce logging
+   io.set("transports", [
+      "websocket",
+      "xhr-polling",
+      "jsonp-polling" 
+   ]);
+});
--- node-with-KS/7.server.js
+++ node-with-KS/7.server.js
@@ -1,12 +1,11 @@
 function handleHTTP(req, res){
-    
     if(req.method === "GET"){
-        
-        if(req.url === '/'){
-            res.writeHead(200, {
-            "Content-type": "text/plain" 
+        if(/^\/\d+(?=$|[\/?#])/.test(req.url)){
+            req.addListener("end", function () {
+                req.url = req.url.replace(/^\/(\d+).*$/,"/$1.html")
+                static_files.serve(req, res);
             });
-            res.end("Hello World "+ Math.random());
+            req.resume();
         } else {
             res.writeHead(403);
             res.end("Not here. Move on!");            
@@ -26,3 +25,10 @@ var port = 8006;
 var http = require('http');
 var http_serv = http.createServer(handleHTTP).listen(port, host);
 
+var ASQ = require("asynquence");
+var node_static = require("node-static");
+
+var static_files = new node_static.Server(__dirname);
+var io = require('socket.io');
+
+io.listen(http_serv);
