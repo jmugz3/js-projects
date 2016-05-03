c5a1fe40e57eeb88b9da8b1f34f42594c03157b4
Jose Mugaburu <mugab686@gmail.com>
2016-05-03 14:21:19 -0400

    refactors recursion exercise

--- nodeschool/functional-javascript/basicRecursion.js
+++ nodeschool/functional-javascript/basicRecursion.js
@@ -1,6 +1,12 @@
 'use strict';
 
 
-module.exports = function reduce(arr, fn, init){
+module.exports = function reduce(arr, fn, init) {
     
-}
+    return (function recursion(index, value){
+        //exit before index out of range
+        if (index > arr.length - 1) return value;
+        return recursion(index + 1 , fn(value, arr[index], index, arr))  
+    })(0, init)
+   
+}    
