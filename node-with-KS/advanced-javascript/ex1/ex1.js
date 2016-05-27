783fc40c2bcc68e34967cda6abca6a7117354bb1
Jose Mugaburu <jmugz3@users.noreply.github.com>
2016-05-27 09:08:08 -0400

    adds instructions for exercise 1

--- node-with-KS/advanced-javascript/ex1/ex1.js
+++ node-with-KS/advanced-javascript/ex1/ex1.js
@@ -1,3 +1,21 @@
+/*
+Instructions
+Fix the code so it prints out the alphabet A-Z in the console.
+
+Cannot:
+- Have any global variables at all
+- Delete or combine any function declarations
+- Create any new functions (except IIFEs -- hint!)
+- Rearrange the order of declarations
+
+Can/must:
+- Declare extra variables (as long as they're not global)
+- Modify (in-place) function declaration/initialization
+- Add/remove statements/expressions (IIFEs, return, params, etc)
+- Make the fewest changes possible
+*/
+
+
 A();
 
 function C() {
