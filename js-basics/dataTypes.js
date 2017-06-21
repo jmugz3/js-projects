0967e514f7e112f1b2b14f280959505a26f6ff6c
Jose Mugaburu <mugab686@gmail.com>
2017-06-21 00:01:09 -0400

    updates variable file and adds dataType file

---
+++ js-basics/dataTypes.js
@@ -0,0 +1,31 @@
+/**
+ * Strings
+ */
+
+
+/**
+ * Numbers
+ * No distinction between floats and integers
+ */
+var score = 850;
+var percentage = 98.99;
+console.log(typeof(score));
+console.log(typeof(percentage));
+console.log( typeof(score) == typeof(percentage) );
+
+/**
+ * Null
+ */
+
+
+/**
+ * undefined
+ */
+
+/**
+ * boolean
+ */
+
+/**
+ * Objects
+ */
--- js-basics/variables.js
+++ js-basics/variables.js
@@ -1,22 +1,71 @@
 /**
- * Covers variables and comments
+ * Declaration and Initialization
  */
 
-var x = "test";
+// 1. Variables name must be unique
+// 2. Can begin with letters, $ (jquery) or _ (private)
+// 3. Can contain number, letters
+// 4. Are case sensitive
+// 4. Cannot be reserved words
+
+
+// Declaration and Initialization
+// Declaration registers with scope
+// Initialization allocates memory
+var x;
+var x1;
+var X;
+var X1;
+// What value does each one of these contain? 
 console.log(x);
 
+// Assignment
+// assigns a value to the variable
+x = "test";
 var y = 12345;
+console.log(x);
 console.log(y);
 
 /**
- * Run as variables.js in terminal 
- * to show it won't work
- */
-
-/**
+ * HOW TO RUN JAVASCRIPT ? 
  * You need node.js, a REPL, or the JS console.
  * Alt: Phantom.js
  */
 
-let a = "abc";
-const b = "234";
+// EcmaScript6
+const b;
+b = 234;
+const z = true;
+console.log(b);
+console.log(z);
+
+// Block Scope
+let a;
+console.log(a);
+a = "abc"
+console.log(a);
+
+
+var foo, bar;
+foo = "myFoo";
+bar = "myBar";
+
+/**
+ * Reserved word in Javascript
+ */
+
+// // data type
+// string, boolean, number, null, undefined, object
+
+// // control flow and loops
+// continue, break, for, return, do, while, if, else, in
+// try, catch, finally
+
+// // declaration and instantiation
+// var, let, const, new, char
+
+// // values
+// true, false,
+
+// // others
+// typeof
