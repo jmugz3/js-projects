c99ffc0eef9827426a051dade70ac53e1d9bdc52
Jose Mugaburu <mugab686@gmail.com>
2017-06-25 22:37:08 -0400

    adds patterns.js as a skeleton for examples

---
+++ js-basics/patterns.js
@@ -0,0 +1,33 @@
+/**
+ *  RECURSION
+ */
+
+
+// function test(x) {
+    // if (x < 1){
+    //     console.log(x);
+    //     return;
+    // }
+    // return test(x -1);
+// }
+
+// test(10);
+
+/**
+ *  MODULE PATTERN
+ */
+
+
+
+/**
+ *  PROTOTYPE PATTERN
+ */
+
+
+/**
+ *  MODULE PATTERN
+ */
+
+/**
+ *  SINGLETON
+ */
