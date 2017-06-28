2560a9ca33e8fe1e1a638a28c26a0bfba0c9075c
Jose Mugaburu <mugab686@gmail.com>
2017-06-28 00:17:15 -0400

    adds null and undefined examples. Also fixes string examples

--- js-basics/dataTypes.js
+++ js-basics/dataTypes.js
@@ -3,46 +3,65 @@
  * Are Immutable
  */
 
-var language = "javascript";
-"javascript".length;
-language.length;
-language.concat("is my new favorite thing");
-console.log(language);
-console.log(language.concat(" is my new favorite thing"));
+// var language = 'javascript';
+// 'javascript'.length;
+// language.length;
+// language.concat("is my new favorite thing");
+// console.log(language);
+// console.log(language.concat(" is my new favorite thing"));
 
-var last2 = language.substring(language.length - 2);
-console.log(last2);
-console.log(`language.charAt(0): ${language.charAt(0)}`);
-console.log(`language.charAt(language.length): ${language.charAt(language.length)}`);
-console.log(`language.charAt(language.length - 1): ${language.charAt(language.length - 1)}`);
+// var last2 = language.substring(language.length - 2);
+// console.log(last2);
+// console.log(`language.charAt(0): ${language.charAt(0)}`);
+// console.log(`language.charAt(language.length): ${language.charAt(language.length)}`);
+// console.log(`language.charAt(language.length - 1): ${language.charAt(language.length - 1)}`);
 
-// print half the word, then the other half
-console.log(language.substring(language.length // 2))
+// language.substring(0, 5);
+// // print half the word, then the other half
+// language.substring(Math.floor(language.length/2), language.length - 1);
 
 
 /**
  * NUMBERS
  * No distinction between floats and integers
  */
-var score = 850;
-var percentage = 98.99;
-console.log(typeof(score));
-console.log(typeof(percentage));
-console.log( typeof(score) == typeof(percentage) );
+
+// const score = 850;
+// const percentage = 98.99;
+// typeof(score) === 'number';
+// console.log( typeof(percentage));
+// console.log( typeof(score) == typeof(percentage) );
 
 /**
- * Null
+ * NULL
  */
 
+// console.log(null);
+// typeof(null);
+// null == undefined // true
+// null === undefined // false
+
 
 /**
- * undefined
+ * UNDEFINED
+ */
+
+// console.log(undefined);
+// typeof(undefined);
+// z === undefined; // reference error
+
+// var t3;
+// t3 == null; // true
+// t3 === null; // false
+// typeof(t3);
+// t3 == undefined;
+// t3 === undefined;
+
+
+/**
+ * BOOLEAN
  */
 
 /**
- * boolean
- */
-
-/**
- * Objects
+ * OBJECTS
  */
