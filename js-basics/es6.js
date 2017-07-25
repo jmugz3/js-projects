64ddbb75cd7f161b8879d295b034305d13677970
Jose Mugaburu <mugab686@gmail.com>
2017-07-25 00:33:34 -0400

    adds chaining promises

--- js-basics/es6.js
+++ js-basics/es6.js
@@ -12,9 +12,32 @@ function tellme() {
 }
 
 
-tellme.then(function(message) {
+tellme().then(function(message) {
   console.log(message);
 })
 .catch(function(err) {
   console.log(err);
 })
+
+
+// CHAINING PROMISES
+
+function add2(num) {
+  return new Promise(function(response, request){
+    if (num && num > 0) {
+      resolve(num + 2);
+    } else {
+      reject(Error("This is not a number"))
+    }
+  });
+}
+
+add2(5).then(function(number){
+  return number + 3;
+})
+.then(function(number){
+  return number + 4;
+})
+.catch(function(err){
+  console.log(err);
+});
