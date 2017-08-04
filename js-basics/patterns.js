b6bde1fcc5d0bbc4d7d8669a022e276522de50da
Jose Mugaburu <mugab686@gmail.com>
2017-08-04 00:13:55 -0400

    another example for recursion

--- js-basics/patterns.js
+++ js-basics/patterns.js
@@ -3,15 +3,22 @@
  */
 
 
-// function test(x) {
-    // if (x < 1){
-    //     console.log(x);
-    //     return;
-    // }
-    // return test(x -1);
-// }
+function test(x) {
+    if (x < 1){
+        console.log(x);
+        return;
+    }
+    return test(x -1);
+}
 
 // test(10);
+
+function test2(y) {
+    console.log(y);
+    return y > 1 ? test2(y - 1): console.log(0);
+}
+
+test2(5);
 
 /**
  *  MODULE PATTERN
